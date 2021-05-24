b854e50b5c9950ad44f1c53a95c7ce0a8ea49c03
uroge <67282212+uroge@users.noreply.github.com>
2021-05-24 13:01:34 +0200

    Created modal for adding events

---
+++ src/script/modal.js
@@ -0,0 +1,32 @@
+export default class Modal {
+    constructor(modalElement, isUpdate, isNewEvent, title) {
+        this.modalElement = modalElement;
+        this.isUpdate = isUpdate;
+        this.isNewEvent = isNewEvent;
+        this.title = title;
+
+        console.log(isUpdate);
+        if(this.isUpdate) {
+            this.title ? this.title.textContent = 'Update Event' : null;
+        }
+
+        console.log(isNewEvent);
+        if(this.isNewEvent) {
+            this.title ? this.title.textContent = 'New Event' : null;
+        }
+    }
+
+    /**
+     * Function that shows modal
+    */
+    showModal() {
+        this.modalElement.classList.replace('hide', 'show');
+    }
+
+    /**
+     * Function that closes modal
+    */
+    closeModal() {
+        this.modalElement.classList.replace('show', 'hide');
+    }
+}
--- src/script/script.js
+++ src/script/script.js
@@ -1,28 +1,32 @@
 import '../sass/main.scss';
+import Modal from './modal';
 
 (function() {
     const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
         prevMonthButton = document.querySelector('.js-main__arrow-left'),
         nextMonthButton = document.querySelector('.js-main__arrow-right'),
         mainHeader = document.querySelector('.js-main__header h1'),
-        calendar = document.querySelector('.js-main__calendar');
+        calendar = document.querySelector('.js-main__calendar'),
+        modalElement = document.querySelector('.js-modal'),
+        modalTitle = document.querySelector('.js-modal__title'),
+        modalSaveButton = document.querySelector('.js-modal__save'),
+        modalCloseButton = document.querySelector('.js-modal__close'),
+        modalInput = document.getElementById('eventTitleInput');
 
-    let currentMonth = 0;
+    let currentMonth = 0,
+        clicked = null,
+        events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [],
+        updateEventModal = new Modal(modalElement, true, false, modalTitle),
+        newEventModal = new Modal(modalElement, false, true, modalTitle);
 
-    const checkMonth = () => {
-        switch (currentMonth) {
-            case 0: mainHeader.textContent = 'January'
-                break;
-            case 1: mainHeader.textContent = 'February'
-                break;
-            case 2: mainHeader.textContent = 'March'
-                break;
-            case 3: mainHeader.textContent = 'April'
-                break;
-            default:
-                break;
+
+    document.body.addEventListener('click', event => {
+        let target = event.target;
+
+        if(target.classList.contains('main__new-event')) {
+            // openModal();
         }
-    };
+    });
 
     /**
      * A function that calculates current day, month and year
@@ -48,7 +52,7 @@ import '../sass/main.scss';
         passiveDays = weekdays.indexOf(dateString.split(', ')[0]);
 
         if(mainHeader) {
-            mainHeader.textContent = `${date.toLocaleDateString('en-GB', {month: 'long'})} ${year}`   
+            mainHeader.textContent = `${date.toLocaleDateString('en-GB', {month: 'long'})} ${year}`;   
         }
 
         calendar.innerHTML = '';
@@ -57,26 +61,64 @@ import '../sass/main.scss';
             const dayElement = document.createElement('div');
             dayElement.classList.add('main__day');
             const dayHolder = document.createElement('p');
-
-
-            dayElement.addEventListener('click', () => {console.log(i, passiveDays)});
+            const newEventIcon = document.createElement('i');
+            newEventIcon.classList.add('fas', 'fa-plus-circle', 'main__new-event');
 
             if(i > passiveDays) {
                 dayElement.appendChild(dayHolder);
                 dayHolder.innerText = i - passiveDays;
+                dayElement.appendChild(newEventIcon);
 
-                if(i === passiveDays + day) {
-                    dayHolder.classList.add('current-day');
+                if(newEventIcon) {
+                    newEventIcon.addEventListener('click', () => {
+                        openModal(`${i - passiveDays}/${month + 1}/${year}`)
+                    });
                 }
 
-                dayElement.addEventListener('click', () => {console.log('click')});
+                if(currentMonth === 0) {
+                    if(i === passiveDays + day) {
+                        dayHolder.classList.add('current-day');
+                    }
+                }
+
             } else {
                 dayElement.classList.add('main__day-passive');
             }
 
             calendar.appendChild(dayElement);
         }
-    }
+    };
+
+
+    /**
+     * Function that opens modal
+    */
+    const openModal = (date) => {
+        clicked = date;
+
+        const eventForDay = events.find(event => event.date === clicked);
+
+        if(eventForDay) {
+            console.log('Event already exists');
+        } else {
+            newEventModal.showModal();
+        }
+    };
+
+    const saveEvent = () => {
+        if(modalInput.value) {
+            modalInput.classList.remove('error');
+            if(!events.find(event => event.date === clicked)){
+                events.push({
+                    date: clicked,
+                    title: modalInput.value
+                });
+            }
+            console.log(events);
+        } else {
+            modalInput.classList.add('error');
+        }
+    };
 
     /**
      * Function that adds event listeners to the
@@ -104,7 +146,24 @@ import '../sass/main.scss';
                 calculateCalendar();
             });
         }
-    }
+
+        /**
+         * Function that closes modal when x button is clicked 
+        */
+        if(modalCloseButton) {
+            modalCloseButton.addEventListener('click', () => {
+                modalInput.classList.remove('error');
+                newEventModal.closeModal();
+            });
+        }
+
+        /**
+         * Function that closes modal when x button is clicked 
+        */
+        if(modalSaveButton) {
+            modalSaveButton.addEventListener('click', saveEvent);
+        }
+    };
 
     initButtons();
     calculateCalendar();
